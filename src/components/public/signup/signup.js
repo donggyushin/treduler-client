6ffff9f8470c925649906e9316e6d5748ad11d28
shin donggyu <donggyu9410@gmail.com>
2019-08-08 16:48:33 +0900

    asd

--- src/components/public/signup/signup.js
+++ src/components/public/signup/signup.js
@@ -41,7 +41,7 @@ width: 97.5%;
 const CreateNewAccountButton = styled.button`
     width: 100%;
     /* text-align: center; */
-    background: #EDEFF0;
+    background:${props => props.disabled ? `#EDEFF0` : `#27ae60`};
     border-radius: 4px;
     border: 1px solid #CDD2D4;
     height: 41px;
@@ -49,12 +49,20 @@ const CreateNewAccountButton = styled.button`
     justify-content: center;
     align-items: center;
     font-weight: bold;
-    color:rgb(140, 140, 140);
+    color:${props => props.disabled ? `rgb(140, 140, 140)` : `white`};
     font-size:23px;
 `
 
 class SignUp extends React.Component {
+    state = {
+        allInputNotChecked: true,
+        name: "",
+        email: "",
+        password: ""
+    }
     render() {
+        const { allInputNotChecked, name, email, password } = this.state;
+        const { handleInput } = this;
         return (
             <Container>
                 <Card>
@@ -62,12 +70,12 @@ class SignUp extends React.Component {
                         Create a Trello Account
                 </BigText>
                     <NormalText>Name</NormalText>
-                    <Input placeholder={'e.g., Guybrush Threepwood'} />
+                    <Input name={'name'} onChange={handleInput} value={name} placeholder={'e.g., Guybrush Threepwood'} />
                     <NormalText>Email</NormalText>
-                    <Input placeholder={'e.g., threepwood@grogmail.com'} />
+                    <Input name={'email'} onChange={handleInput} value={email} placeholder={'e.g., threepwood@grogmail.com'} />
                     <NormalText>{'Password'}</NormalText>
-                    <Input placeholder={'e.g., **********'} />
-                    <CreateNewAccountButton disabled={true}>
+                    <Input name={'password'} onChange={handleInput} value={password} placeholder={'e.g., **********'} />
+                    <CreateNewAccountButton disabled={allInputNotChecked}>
                         Create New Account
                 </CreateNewAccountButton>
                 </Card>
@@ -75,6 +83,25 @@ class SignUp extends React.Component {
             </Container>
         )
     }
+
+    handleInput = (e) => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        })
+
+        if (this.state.name.length >= 3 && this.state.email.length >= 5 && this.state.password.length >= 5) {
+            this.setState({
+                allInputNotChecked: false
+            })
+        } else {
+            this.setState({
+                allInputNotChecked: true
+            })
+        }
+
+    }
+
 }
 
 export default SignUp
