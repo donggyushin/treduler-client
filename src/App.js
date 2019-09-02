fb3c731a32db87510e7e4be233061a7aee8f1f17
shin donggyu <donggyu9410@gmail.com>
2019-09-02 18:57:09 +0900

    distinguish dev and prod

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import 'antd/dist/antd.css'
 
 class AppContainer extends React.Component {
 
+
+
+
   render() {
     const { isLoggedIn } = this.props.authentication;
     return <App isLoggedIn={isLoggedIn} />
--- src/constants/endpoint.js
+++ src/constants/endpoint.js
@@ -1,1 +1,8 @@
-export const ENDPOINT = "http://127.0.0.1"
+let URL = "http://www.treduler.nl"
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    // dev code
+    URL = "http://127.0.0.1"
+}
+
+export const ENDPOINT = URL
